15030a33edf8fc97fa5d7aba85f6a3f3e5362d96
Sunnyhaze <mxch1122@126.com>
2026-03-10 21:59:29 +0800

    [v0.0.4] Boost version

--- flash_mineru/ray_utils/__init__.py
+++ flash_mineru/ray_utils/__init__.py
@@ -1,7 +1,6 @@
 from .dispatch_mode import DispatchMode, get_predefined_dispatch_fn, Dispatch
 from .dispatch_ray_module import RayModule
 
-# from env_registry import xxx  --- IGNORE ---
 
 __all__ = [
     # RayModule Core
@@ -10,6 +9,5 @@ __all__ = [
     'DispatchMode',
     'get_predefined_dispatch_fn',
     'Dispatch',
-    # Registry
-    # 'xxx',  --- IGNORE ---
+
 ]
